fc17d1bf0b564bb216321225935cb15190860469
Anihithe <Anihithe@gmail.com>
2021-07-08 14:14:13 +0200

    Init
    Init Project with MaterialDesgin

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace ClassGenetics
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
